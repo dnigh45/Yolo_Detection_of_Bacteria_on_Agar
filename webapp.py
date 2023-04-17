fefc988be0f624f142695da62bbbbe69d3d67877
DJ Nigh <djpnigh@gmail.com>
2023-04-17 16:28:52 -0400

    Uploading file notebook and modding others

--- webapp.py
+++ webapp.py
@@ -11,31 +11,32 @@ from collections import defaultdict
 import json
 import webcolors
 
-model = YOLO('models_train/97epoch.pt')
-UPLOAD_FOLDER = 'upload_folder'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-def allowed_file(filename):
+model = YOLO('models_train/97epoch.pt') # instantiate the model
+UPLOAD_FOLDER = 'upload_folder' # define were files will be saved
+os.makedirs(UPLOAD_FOLDER, exist_ok=True) # create directory for save if none there
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'} # define allowed files
+app = Flask(__name__) #instatiate flask application
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # set upload folder
+def allowed_file(filename): # check if uploaded file has an allowed extension
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']) #define GET and POST methods
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file' not in request.files: # if no file is submitted
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if file and allowed_file(file.filename): # if there is a file and it's allowed continue
+            filename = secure_filename(file.filename) # ensure safe file
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))# save file
             # img = Image.open(f'{UPLOAD_FOLDER}/{filename}')
-            img = cv2.imread(f'{UPLOAD_FOLDER}/{filename}')
-            output_img, classes, colorHex = inference_img(img)
-            _, buffer = cv2.imencode('.jpg', output_img)
-            b64_img = b64encode(buffer).decode()
-            return f'''
+            img = cv2.imread(f'{UPLOAD_FOLDER}/{filename}') # open file
+            output_img, classes, colorHex = inference_img(img) # perform inference
+            _, buffer = cv2.imencode('.jpg', output_img) #encode image from jpg
+            b64_img = b64encode(buffer).decode() # encode butter file for html display
+            ## HTML for post
+            return f''' 
             <!doctype html>
             <h1> Your Agar Plate with Detections </h1>
             <img src=data:image/jpeg;base64,{b64_img} width="960" height="960">
@@ -62,10 +63,11 @@ def upload_file():
         <input type=submit value=Upload>
         </form>
         '''
-def inference_img(img: np.array) -> np.array:
+def inference_img(img: np.array) -> np.array: # take in image as numpy array and return a numpy array
     imgsize = 1280
-    results = model.predict(img, imgsz=imgsize)
-    output_img = img.copy()
+    results = model.predict(img, imgsz=imgsize) #define image size
+    output_img = img.copy() # copy image for bbox drawing
+    # define classes
     class_maps = [
         "S.aureus",
         "B.subtilis", 
@@ -73,44 +75,27 @@ def inference_img(img: np.array) -> np.array:
         "E.coli ",
         "C.albicans",
     ]
-    colorsRGB = matplotlib.cm.tab20(range(len(class_maps)))
-    colors = [(i[:-1][::-1]*255) for i in colorsRGB]
-    colorsRev = [(i[:-1][::1]*255) for i in colorsRGB]
-    colorsTuple = [(int(x),int(y),int(z)) for x,y,z in colorsRev]
-    colorHex = {x:webcolors.rgb_to_hex(y) for x, y in zip(class_maps, colorsTuple)}
+    colorsRGB = matplotlib.cm.tab20(range(len(class_maps))) # import colors
+    colors = [(i[:-1][::-1]*255) for i in colorsRGB] # convert colors to portion of 255 in BGR format
+    colorsRev = [(i[:-1][::1]*255) for i in colorsRGB] # convert colors to portion of 255 in RGB format
+    colorsTuple = [(int(x),int(y),int(z)) for x,y,z in colorsRev] # convert form list to tuple
+    colorHex = {x:webcolors.rgb_to_hex(y) for x, y in zip(class_maps, colorsTuple)} # convert RBG to hex code
     print(colors)
-    classes_found = defaultdict(int)
-    for result in results:
+    classes_found = defaultdict(int) # instaiate dictionary with found colonies
+    for result in results: # take bboxes and draw them on to image
         boxes = result.boxes.to('cpu').numpy()
         classes = boxes.cls.astype(int)
         for box, cls in zip(boxes, classes):
             bbox_class = class_maps[cls]
-            coord = box.xyxy.astype(int).squeeze()
+            coord = box.xyxy.astype(int).squeeze() # return bbox coord in smallest format
             xmin, ymin, xmax, ymax = coord
-            classes_found[bbox_class] += 1
+            classes_found[bbox_class] += 1 # count colonies
             
-            color = colors[cls]
-            color = tuple(color)
-            #color = (0, 0, 255) 
-            #font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-            #font_scale = .5
-            #font_thickness = 1
-            #txt_size = cv2.getTextSize(bbox_class, font, font_scale,
-            #                           font_thickness)  # gets the width and height of the string
-            #bbox_x_2 = (xmax - xmin) // 2 + xmin  # gets the xmin + (w/2) --> center x coord of the bbox
-            #txt_w_2 = txt_size[0][0] // 2  # extracts the width from the txt and divides by two
-            #txt_loc = (bbox_x_2 - txt_w_2, ymin - 10)  # puts the text at the top center of the bbox
-            #cv2.putText(output_img, bbox_class, txt_loc, font, font_scale, color, font_thickness, cv2.LINE_AA)
-            cv2.rectangle(output_img, (xmin, ymin), (xmax, ymax), color, 2)
+            color = colors[cls] # define color by class
+            color = tuple(color) # convert color to a tuple
+
+            cv2.rectangle(output_img, (xmin, ymin), (xmax, ymax), color, 2) # draw rectangle on image
     print(classes_found)
-    return output_img, classes_found, colorHex
-if __name__ == '__main__':
+    return output_img, classes_found, colorHex # return image with bboxes, colony count, and hex values for colors used
+if __name__ == '__main__': # run application
     app.run()
-    # img = cv2.imread(f'{UPLOAD_FOLDER}/935.jpg')
-    # inferenced_img = inference_img(img)
-    
-    
-    # cv2.imshow('test', cv2.resize(inferenced_img, (960, 960)))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.imwrite('test.jpg')
